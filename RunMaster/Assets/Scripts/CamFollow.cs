a503efcbca5b42f7e3e5d09abb9bfa59aa4c35c3
celikcagdas <celikkcagdas@gmail.com>
2024-11-03 03:11:42 +0300

    Game Design

---
+++ RunMaster/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 target_offset;
+    void Start()
+    {
+        //kameranýn pozisyonundan hedefimin pozisyonunu çýkarararak hedefimi buluyorum
+        target_offset = transform.position - target.position;
+    }
+    private Vector3 velocity = Vector3.zero;
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + target_offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.3f);
+    }
+}
